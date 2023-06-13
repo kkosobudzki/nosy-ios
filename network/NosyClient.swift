2f93421d92a25a133f5a646c031de922d815e9df
Krzysztof Kosobudzki <krzysztof@koia.consulting>
2023-06-13 18:42:55 +0200

    Initial framework commit

---
+++ network/NosyClient.swift
@@ -0,0 +1,98 @@
+//
+//  NosyClient.swift
+//  WongaPay
+//
+//  Created by Krzysztof Kosobudzki on 12/06/2023.
+//
+
+import Foundation
+import GRPC
+import NIO
+
+class NosyClient {
+  var service: Nosy_LoggerAsyncClient?
+  
+  let port: Int = 6969
+  
+  init() {
+    let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    
+    do {
+      let channel = try GRPCChannelPool.with(
+        target: .host("localhost", port: self.port),
+        transportSecurity: .plaintext,
+        eventLoopGroup: eventLoopGroup
+      )
+      
+      self.service = Nosy_LoggerAsyncClient(channel: channel)
+      
+      print("Nosy connection initialized")
+    } catch {
+      print("Could not connect to Nosy")
+    }
+  }
+  
+  func logRequest(_ request: URLRequest) async -> String? {
+    let networkRequest: Nosy_NetworkRequest = .with {
+      $0.method = request.httpMethod ?? "Unknown http method"
+      $0.url = request.url?.absoluteString ?? "Unknown url"
+      $0.headers = request.allHTTPHeaderFields ?? [:]
+      $0.body = request.httpBodyStream?.readfully().string() ?? ""
+    }
+    
+    do {
+      let registerdRequest = try await service?.logRequest(networkRequest)
+      
+      return registerdRequest?.id
+    } catch {
+      print("Failed to log request to Nosy. Got error: \(error)")
+    }
+    
+    return nil
+  }
+  
+  func logResponse(_ requestId: String, _ response: URLResponse, _ data: Data) async -> Void {
+    guard let response = response as? HTTPURLResponse else { return }
+    
+    let networkResponse: Nosy_NetworkResponse = .with {
+      $0.requestID = requestId
+      $0.code = Int32(response.statusCode)
+      $0.headers = response.allHeaderFields as NSDictionary? as? [String: String] ?? [:]
+      $0.body = data.string()
+    }
+    
+    do {
+      try await service?.logResponse(networkResponse)
+    } catch {
+      print("Failed to log response to Nosy. Got error: \(error)")
+    }
+  }
+}
+
+extension InputStream {
+  func readfully() -> Data {
+    var result = Data()
+    var buffer = [UInt8](repeating: 0, count: 4096)
+        
+    open()
+        
+    var amount = 0
+    repeat {
+      amount = read(&buffer, maxLength: buffer.count)
+            
+      if amount > 0 {
+        result.append(buffer, count: amount)
+      }
+    } while amount > 0
+        
+    close()
+        
+    return result
+  }
+}
+
+extension Data {
+  func string() -> String {
+    return String(data: self, encoding: String.Encoding.utf8) ?? "Could not create data string"
+  }
+}
---
+++ network/network.h
@@ -0,0 +1,18 @@
+//
+//  network.h
+//  network
+//
+//  Created by Krzysztof Kosobudzki on 13/06/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for network.
+FOUNDATION_EXPORT double networkVersionNumber;
+
+//! Project version string for network.
+FOUNDATION_EXPORT const unsigned char networkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <network/PublicHeader.h>
+
+
