4ad839fc3d0dd67a8365a1c141c3877ea80d43cc
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2023-06-13 19:51:58 +0200

    Fix configuration

--- network/network.h
+++ network/network.h
@@ -1,18 +1,18 @@
 //
-//  network.h
-//  network
+//  dupa.h
+//  dupa
 //
 //  Created by Krzysztof Kosobudzki on 13/06/2023.
 //
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for network.
-FOUNDATION_EXPORT double networkVersionNumber;
+//! Project version number for dupa.
+FOUNDATION_EXPORT double dupaVersionNumber;
 
-//! Project version string for network.
-FOUNDATION_EXPORT const unsigned char networkVersionString[];
+//! Project version string for dupa.
+FOUNDATION_EXPORT const unsigned char dupaVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <network/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <dupa/PublicHeader.h>
 
 
