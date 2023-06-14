d8a66da8e573132e102e90c46a9d0476218d3b49
Krzysztof Kosobudzki <krzysztof@koia.consulting>
2023-06-14 11:26:39 +0200

    Extract classes to separate files

---
+++ logging/LoggingInterceptor.swift
@@ -0,0 +1,54 @@
+//
+//  LoggingInterceptor.swift
+//  NosyNetwork
+//
+//  Created by Krzysztof Kosobudzki on 14/06/2023.
+//
+
+import Foundation
+
+class LoggingInterceptor : URLProtocol {
+  private static let FlagKey = "NosyIntercepted"
+  
+  private let nosy: NosyClient = NosyClient()
+  
+  override class func canInit(with request: URLRequest) -> Bool {
+    guard self.property(forKey: FlagKey, in: request) == nil
+    else { return false } // already intercepted
+    
+    return true
+  }
+  
+  override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+    return request
+  }
+  
+  override func startLoading() {
+    guard let client = self.client else {
+      return
+    }
+    
+    let mutableRequest = (self.request as NSURLRequest).mutableCopy() as! NSMutableURLRequest
+    let request = mutableRequest as URLRequest
+    
+    LoggingInterceptor.setProperty(true, forKey: LoggingInterceptor.FlagKey, in: mutableRequest)
+    
+    Task.init {
+      let requestId = await self.nosy.logRequest(request)
+        
+      let (data, response) = try await URLSession.shared.data(for: request)
+      
+      if let id = requestId {
+        await self.nosy.logResponse(id, response, data)
+      }
+      
+      client.urlProtocol(self, didReceive: response, cacheStoragePolicy: URLCache.StoragePolicy.allowed)
+      client.urlProtocol(self, didLoad: data)
+      client.urlProtocolDidFinishLoading(self)
+    }
+  }
+  
+  override func stopLoading() {
+    // no-op
+  }
+}
