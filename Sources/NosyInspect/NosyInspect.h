c09b8422cd7fad9d05c5d2a1e573cc293556e567
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2023-09-13 13:15:45 +0200

    Working SPM version

---
+++ Sources/NosyInspect/NosyInspect.h
@@ -0,0 +1,17 @@
+//
+//  logging.h
+//
+//  Created by Krzysztof Kosobudzki on 13/06/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for logging.
+FOUNDATION_EXPORT double loggingVersionNumber;
+
+//! Project version string for logging.
+FOUNDATION_EXPORT const unsigned char loggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <logging/PublicHeader.h>
+
+
